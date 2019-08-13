e84154020df103b69fc20d8a77f98542eb8a70d6
batTrung <trungbat.pv@gmail.com>
2019-08-13 15:16:26 +0700

    Heroku config

--- myproject/settings.py
+++ myproject/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
